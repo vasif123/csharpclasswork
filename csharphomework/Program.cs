4d4dece3e355969e86f0ac20c9c74f3ca1791cc0
vasif123 <vasifaha@code.edu.az>
2022-04-12 02:51:21 +0400

    homework

---
+++ csharphomework/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace csharphomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User name daxil edin");
+            string username = Console.ReadLine();
+            //int minlength = 8;
+            //while (username.Length >= minlength)
+            //{
+            //    Console.WriteLine("minumum 8 simvollu  daxil et");
+            //}
+            Console.WriteLine("Parol daxil et");
+            string password = Console.ReadLine();
+            userdata usercontrol = new userdata();
+            usercontrol.User = username;
+            usercontrol.Password = password;
+            Console.WriteLine(usercontrol.User);
+        }
+    }
+}
---
+++ csharphomework/userdata.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharphomework
+{
+    class userdata
+    {
+        private string _user;
+        private string _password;
+        
+        public string User
+        {
+            get
+            {
+                return _user;
+            }
+            set
+            {
+
+            }
+        }
+
+         public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                int count = 0;
+                int count1 = 0;
+                int count2 = 0;
+                if (value.Length >= 8)
+                {
+                    foreach (char item in value)
+                    {
+                        if (char.IsUpper(item))
+                        {
+                            count++;
+                        }
+                        else if (char.IsDigit(item))
+                        {
+                            count1++;
+                        }
+                        else if (char.IsLower(item))
+                        {
+                            count2++;
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+}
